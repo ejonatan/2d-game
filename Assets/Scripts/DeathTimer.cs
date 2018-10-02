86ed8171079eadcbbffafa70bb775163151a8631
Emily Jonatan <jonatan@email.arizona.edu>
2018-10-01 22:28:50 -0700

    Added animations and feeding scripts
    Animations for good and evil peppermints, script for feeding, script for killing projectile

---
+++ Assets/Scripts/DeathTimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTimer : MonoBehaviour {
+
+	public float lifetime;
+	float currentLife;
+
+	public Feed owner;
+
+	void Update () {
+		currentLife += Time.deltaTime;
+
+		if( currentLife > lifetime ) {
+			Destroy( this.gameObject );
+			owner.SugarDestroyed( this );
+		}
+	}
+}
---
+++ Assets/Scripts/Feed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feed : MonoBehaviour {
+
+	public DeathTimer sugar;
+	public float sugarSpawnDist;
+
+	public int maxSugars;
+
+	public List<DeathTimer> sugars = new List<DeathTimer>();
+	
+	void Update () {
+		if( Input.GetButtonDown("Jump") ) {
+			if( sugars.Count > maxSugars ) return;
+			
+			DeathTimer newSugar = Instantiate<DeathTimer>( sugar );
+			newSugar.transform.position = transform.position + transform.right * sugarSpawnDist;
+			sugars.Add( newSugar );
+			newSugar.owner = this;
+		}
+	}
+
+	public void SugarDestroyed( DeathTimer dyingSugar ) {
+		sugars.Remove( dyingSugar );
+	}
+}
