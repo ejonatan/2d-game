6edf622f83788a77aa20bb3c994af2b82270e01f
Emily Jonatan <jonatan@email.arizona.edu>
2018-10-01 21:51:26 -0700

    Repairing animation clips in new hierarchy order

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,57 +9,51 @@ public class PlayerController : MonoBehaviour {
     public Rigidbody2D player;
     public int health = 3;
 
-    bool facingRight = true;
     bool onGround = false;
 
-    Animator animator = ;
+    public SpriteRenderer image;
+    public Animator animator;
 
-    void Update()
-    {
-    	if (onGround && Input.GetButtonDown ("Vertical")) {
+    void Update() {
+    	if ( onGround && Input.GetButtonDown ("Vertical") ) {
     		player.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
     	}
 
-    	if (health <= 0) {
+    	if ( health <= 0 ) {
     		print("player died");
     	}
 
-    	if (onGround && Input.GetButtonDown ("FeedButton")) {
+    	if ( onGround && Input.GetButtonDown ("FeedButton") ) {
     		Feed();
     	}
     }
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         float move = Input.GetAxis ("Horizontal");
         player.velocity = new Vector2 (move * speed, player.velocity.y);
 
-        if (move != 0) {
-
+        if ( move != 0 ) {
+        	animator.SetBool( "Run", true );
+        }
+        else {
+        	animator.SetBool( "Run", false );
         }
 
-        if( move > 0 && !facingRight ) {
-        	Flip();
-        }
-        else if( move < 0 && facingRight ) {
-        	Flip();
-        }
-    }
-
-    void Flip() {
-    	facingRight = !facingRight;
-    	Vector3 theScale = transform.localScale;
-    	theScale.x *= -1;
-    	transform.localScale = theScale;
+        if ( move > 0 ) {
+			image.transform.localRotation = Quaternion.identity;
+		}
+		else if ( move < 0 ) {
+			image.transform.localRotation = Quaternion.Euler( 0f, 180f, 0f );
+		}
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag == "Enemy") {
+        if ( collision.gameObject.tag == "Enemy" ) {
             health -= 1;
             print(health);
         }
 
-        if (collision.gameObject.tag == "Ground" && collision.gameObject.transform.position.y <= transform.position.y) {
+        if ( collision.gameObject.tag == "Ground" && collision.gameObject.transform.position.y <= transform.position.y ) {
             onGround = true;
         }
     }
@@ -72,6 +66,5 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Feed() {
-    	Instantiate(Sugar)
     }
 }
